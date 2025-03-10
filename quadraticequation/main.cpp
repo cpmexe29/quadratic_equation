f8c19ebcf131b5758d5a9a2ee64e05f47b1b6dcb
Egor <akinisheve@gmail.com>
2025-03-10 19:45:32 +0300

    Update main.cpp
    function quare

--- quadraticequation/main.cpp
+++ quadraticequation/main.cpp
@@ -3,36 +3,36 @@
 
 using namespace std;
 
-int main() {
+void quare()
+{
     double d, x1, x2;
     int a, b, c;
-
+    
     cout << "Input a: "; cin >> a;
     cout << "Input b: "; cin >> b;
     cout << "Input c: "; cin >> c;
-
+    
     d = pow(b, 2) - 4 * a * c;
-
     cout << "Dis = " << d << " Pow = " << pow(d, 0.5) << endl;
-
+    
     if(a != 0)
     {
         if(d > 0)
         {
             x1 = (-b - pow(d, 0.5)) / (2 * a);
-
+            
             x2 = (-b + pow(d, 0.5)) / (2 * a);
             cout << "First X = " << x1 << endl;
             cout << "Second X = " << x2 << endl;
         }
-
+        
         if(d == 0)
         {
             x1 = -b / (a * 2);
             cout << "x1 = " << x1 << endl;
             cout << "x2 = " << x1 << endl;
         }
-
+        
         if(d < 0)
         {
             d = -d;
@@ -40,16 +40,21 @@ int main() {
             cout << "x2 = " << -b / (2 * a)  << " - " << - pow(d, 0.5) / (2 * a) << "i" << endl;
         }
     }
-
+    
     if(a == 0 && b != 0 && c != 0)
     {
         cout << "X = " << -c / b << endl;
     }
-
+    
     if(a == 0 && b == 0 && c != 0)
     {
         cout << "no results" << endl;
     }
-
-    return 0;
+    
+    
+}
+int main() 
+{
+    quare();
+return 0;
 }
